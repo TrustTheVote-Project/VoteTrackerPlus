a4243f943aa33c8aa8447b0f8c9faffc2604e676
sandy currier <windoverwater@users.noreply.github.com>
2022-10-19 15:46:13 -0400

    Update src/vtp/setup_vtp_demo.py
    Co-authored-by: Ion Y <78449689+ion-oset@users.noreply.github.com>

--- src/vtp/setup_vtp_demo.py
+++ src/vtp/setup_vtp_demo.py
@@ -35,8 +35,8 @@ import sys
 from logging import debug, info
 
 # Local import
-from utils.common import Globals, Shellout
-from utils.election_config import ElectionConfig
+from .utils.common import Globals, Shellout
+from .utils.election_config import ElectionConfig
 
 # Functions
 
